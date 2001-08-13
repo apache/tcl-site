7f8696077a6aae3598d7451c13694a1ab32bfca4
David Welton <davidw@apache.org>
2001-08-13 18:20:20 +0000

    Added 'why Tcl?' page with some Tcl advocacy. Added powered by Tcl logo. PR: Obtained from: Submitted by: Reviewed by:

--- common.tcl
+++ common.tcl
@@ -16,3 +16,23 @@ proc title { {txt "Apache Tcl Project"} } {
     }
 }
     
+proc powered { } {
+    hputs {
+	<table align="center" width="100%">
+	<tr>
+	<td align="left">
+	<a href="http://www.apache.org/"><img src="apache_pb.gif"
+	alt="Powered by Apache" border="0"
+	width="259"
+	height="32"></a>
+	</td>
+	<td align="right">
+	<a href="http://tcl.activestate.com/"><img src="tclp.gif"
+	alt="Powered by Tcl" border="0"
+	width="42"
+	height="64"></a>
+	</td>
+	</tr>
+	</table>
+    }
+}

412f03fdb4fae8b0e303ad7da1a19bb71cb5af42
David Welton <davidw@apache.org>
2001-09-20 12:19:06 +0000

    Updated mod_dtcl directory to use 'powered' command. PR: Obtained from: Submitted by: Reviewed by:

--- common.tcl
+++ common.tcl
@@ -21,13 +21,13 @@ proc powered { } {
 	<table align="center" width="100%">
 	<tr>
 	<td align="left">
-	<a href="http://www.apache.org/"><img src="apache_pb.gif"
+	<a href="http://www.apache.org/"><img src="/apache_pb.gif"
 	alt="Powered by Apache" border="0"
 	width="259"
 	height="32"></a>
 	</td>
 	<td align="right">
-	<a href="http://tcl.activestate.com/"><img src="tclp.gif"
+	<a href="http://www.tcl-tk.net/"><img src="/tclp.gif"
 	alt="Powered by Tcl" border="0"
 	width="42"
 	height="64"></a>

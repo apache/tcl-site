66edfc6660b923691f973593137969d777ff8ccf
David Welton <davidw@apache.org>
2002-07-10 18:11:52 +0000

    Updated examples.

--- websh/examples/ws3tohtml.tcl
+++ websh/examples/ws3tohtml.tcl
@@ -1,7 +1,11 @@
+#!/bin/sh
+# the next line restarts using tclsh \
+    exec tclsh "$0" "$@"
+
 foreach ws3 [glob *.ws3] {
     puts "processing $ws3"
     catch {
 	exec /usr/bin/enscript -h -B -t [file root $ws3] --color -Etcl -B --language=html -o [file rootname $ws3].html $ws3
     }
-    file copy $ws3 "[file rootname $ws3]_ws3.txt"
+    file copy -force $ws3 "[file rootname $ws3]_ws3.txt"
 }

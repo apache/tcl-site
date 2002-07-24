d72c8ee1630db3ac396fdcf7d69bd8dfaca11e41
Ronnie Brunner <ronnie@apache.org>
2002-07-24 14:23:36 +0000

    - no path to enscript (so that it works more generally)

--- websh/examples/ws3tohtml.tcl
+++ websh/examples/ws3tohtml.tcl
@@ -5,7 +5,7 @@
 foreach ws3 [glob *.ws3] {
     puts "processing $ws3"
     catch {
-	exec /usr/bin/enscript -h -B -t [file root $ws3] --color -Etcl -B --language=html -o [file rootname $ws3].html $ws3
+	exec enscript -h -B -t [file root $ws3] --color -Etcl -B --language=html -o [file rootname $ws3].html $ws3
     }
     file copy -force $ws3 "[file rootname $ws3]_ws3.txt"
 }

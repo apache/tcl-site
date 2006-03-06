c3c33635ba9c0a07088aa41837025c5ebb524222
Ronnie Brunner <ronnie@apache.org>
2006-03-06 18:25:29 +0000

    - upgraded to promote 3.6.0b4 - moved downloads to apache mirror

--- websh/buttons.js
+++ websh/buttons.js
@@ -1,30 +1,30 @@
 if (document.images) {
-	var prod_a = new Image();
-	var prod_b = new Image();
-	var docu_a = new Image();
-	var docu_b = new Image();
-	var supp_a = new Image();
-	var supp_b = new Image();
-	var ress_a = new Image();
-	var ress_b = new Image();
-	var down_a = new Image();
-	var down_b = new Image();
+        var prod_a = new Image();
+        var prod_b = new Image();
+        var docu_a = new Image();
+        var docu_b = new Image();
+        var supp_a = new Image();
+        var supp_b = new Image();
+        var ress_a = new Image();
+        var ress_b = new Image();
+        var down_a = new Image();
+        var down_b = new Image();
 
-	prod_a.src ="buttons/b_prod_a.gif";
-	prod_b.src ="buttons/b_prod_b.gif";
-	docu_a.src ="buttons/b_docu_a.gif";
-	docu_b.src ="buttons/b_docu_b.gif";
-	supp_a.src ="buttons/b_supp_a.gif";
-	supp_b.src ="buttons/b_supp_b.gif";
-	ress_a.src ="buttons/b_ress_a.gif";
-	ress_b.src ="buttons/b_ress_b.gif";
-	down_a.src ="buttons/b_down_a.gif";
-	down_b.src ="buttons/b_down_b.gif";
+        prod_a.src ="/websh/buttons/b_prod_a.gif";
+        prod_b.src ="/websh/buttons/b_prod_b.gif";
+        docu_a.src ="/websh/buttons/b_docu_a.gif";
+        docu_b.src ="/websh/buttons/b_docu_b.gif";
+        supp_a.src ="/websh/buttons/b_supp_a.gif";
+        supp_b.src ="/websh/buttons/b_supp_b.gif";
+        ress_a.src ="/websh/buttons/b_ress_a.gif";
+        ress_b.src ="/websh/buttons/b_ress_b.gif";
+        down_a.src ="/websh/buttons/b_down_a.gif";
+        down_b.src ="/websh/buttons/b_down_b.gif";
 }
 
 function swap(img, type) {
 
-	if(document.images) {
-		document[img].src = "buttons/" + type + ".gif";
-	}
+        if(document.images) {
+                document[img].src = "/websh/buttons/" + type + ".gif";
+        }
 }
